185df669762d7c630ab1f961e05789436e5aa232
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-10-31 20:48:43 +0100

    feat: middleware updated and docs added

--- semana-tres/middleware/auth.middleware.js
+++ semana-tres/middleware/auth.middleware.js
@@ -1,12 +1,26 @@
-const jwt = require('jsonwebtoken');
-
+/**
+ * Middleware function for authentication using JWT.
+ * Verifies the presence and validity of a JWT in the request headers.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {void}
+ * @throws {Object} Returns a 401 status with an error message if authentication fails.
+ */
 module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Bearer <token>
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { id: decodedToken.userId };
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.userData = { userId: decoded.userId };
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Authentication failed' });
+    return res.status(401).json({ message: 'Authentication failed' });
   }
 };
