7f4690fcca0ade72683776dc68c31a60eff3485e
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-10-31 20:50:03 +0100

    fix: test were updated and fully functional

--- semana-tres/tests/api.test.js
+++ semana-tres/tests/api.test.js
@@ -1,8 +1,17 @@
 const request = require('supertest');
 const app = require('../server');
 const mongoose = require('mongoose');
+const { startServer, stopServer } = require('../config/server.config');
 
-let token;
+let accessToken;
+
+beforeAll(async () => {
+  server = await startServer(app);
+});
+
+afterAll(async () => {
+  await stopServer();
+});
 
 describe('Authentication', () => {
   test('Create a new user POST /api/users - success', async () => {
@@ -12,19 +21,21 @@ describe('Authentication', () => {
       password: 'password123',
     });
 
-    expect(res.statusCode).toBe(200);
-    console.log('Token:', res.body.token);
-    expect(res.body).toHaveProperty('token');
-    token = res.body.token;
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('accessToken');
+    accessToken = res.body.accessToken;
   });
 
-  test('Create a new user POST /api/users - failure', async () => {
+  test('Create a new user POST /api/users - failure (missing fields)', async () => {
     const res = await request(app).post('/api/users').send({
       email: 'wrong@example.com',
     });
 
-    expect(res.statusCode).toBe(500);
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toBe('Name, email, and password are required');
   });
+
   test('User login POST /api/login - success', async () => {
     const res = await request(app).post('/api/login').send({
       email: 'user@example.com',
@@ -32,9 +43,9 @@ describe('Authentication', () => {
     });
 
     expect(res.statusCode).toBe(200);
-    console.log('Token:', res.body.token);
-    expect(res.body).toHaveProperty('token');
-    token = res.body.token;
+    expect(res.body).toHaveProperty('accessToken');
+    accessToken = res.body.accessToken;
+    console.log(accessToken);
   });
 });
 
@@ -45,12 +56,10 @@ describe('Protected Routes', () => {
   });
 
   test('GET /api/citations - with token', async () => {
-    const res = await request(app).get('/api/citations').set('Authorization', `Bearer ${token}`);
+    const res = await request(app).get('/api/citations').set('Authorization', `Bearer ${accessToken}`);
 
     expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
   });
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
 });
