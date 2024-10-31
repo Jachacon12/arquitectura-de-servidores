6c0c2653c961713017aa0a5eb76e59c34d88e733
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-10-31 20:49:40 +0100

    feat: model updated

--- semana-tres/models/citation.model.js
+++ semana-tres/models/citation.model.js
@@ -2,13 +2,34 @@ const mongoose = require('mongoose');
 
 const citationSchema = new mongoose.Schema(
   {
-    text: { type: String, required: true },
-    author: { type: String, required: true },
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: String,
     source: String,
     year: Number,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
 );
 
 module.exports = mongoose.model('Citation', citationSchema);
